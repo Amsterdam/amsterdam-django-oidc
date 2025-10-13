b7beb0cb3f045ad0de571cd9dfbd9764c3eba621
Youri Westerman <y.westerman@amsterdam.nl>
2025-10-13 13:10:03 +0200

    Change type hint of `args` in stub

--- stubs/mozilla_django_oidc/auth.pyi
+++ stubs/mozilla_django_oidc/auth.pyi
@@ -6,5 +6,5 @@ from amsterdam_django_oidc import Payload
 
 class OIDCAuthenticationBackend(ModelBackend):
     @staticmethod
-    def get_settings(attr: str, *args: tuple[Any, ...]) -> str | None: ...
+    def get_settings(attr: str, *args: Any) -> str | None: ...  # noqa: ANN401
     def verify_token(self, token: str, **kwargs: dict[str, Any]) -> Payload: ...
